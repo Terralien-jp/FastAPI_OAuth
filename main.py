8cfc4b20bf5e189047837b1edba7503da416e421
Terralien-jp <terralienjp@gmail.com>
2022-04-22 00:38:39 +0900

    fix main.py

--- main.py
+++ main.py
@@ -12,6 +12,19 @@ class User(BaseModel):
     full_name: str | None = None
     disabled: bool | None = None
 
-@app.get("/items/")
-async def read_items(token: str = Depends(oauth2_scheme)):
-    return {"token": token}
+# @app.get("/items/")
+# async def read_items(token: str = Depends(oauth2_scheme)):
+#     return {"token": token}
+
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
+    )
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    return user
+
+@app.get("/user/me")
+async def   read_users_me(current_user: User =Depends(get_current_user)):
+    return current_user
